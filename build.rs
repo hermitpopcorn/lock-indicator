2cf8b842e6b3aa8bea53e73f2c21617cc1004ea1
Dani Nugraha <dani.hmng@gmail.com>
2022-12-13 14:59:46 +0700

    switched crate: tray-icon -> nwg

--- build.rs
+++ build.rs
@@ -1,11 +1,4 @@
+extern crate embed_resource;
 fn main() {
-	if cfg!(target_os = "windows") {
-		let mut res = winres::WindowsResource::new();
-		res.set_icon("resource/state3.ico");
-		res.set_icon_with_id("resource/state0.ico", "state0-icon");
-		res.set_icon_with_id("resource/state1.ico", "state1-icon");
-		res.set_icon_with_id("resource/state2.ico", "state2-icon");
-		res.set_icon_with_id("resource/state3.ico", "state3-icon");
-		res.compile().expect("compiling resource file");
-	}
+    embed_resource::compile("embed_resources.rc");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,68 @@
-#![windows_subsystem = "windows"]
+extern crate native_windows_gui as nwg;
+extern crate native_windows_derive as nwd;
 
-use std::sync::mpsc;
-use std::time::Duration;
-use tray_item::TrayItem;
+use core::panic;
+
+use nwd::NwgUi;
+use nwg::NativeUi;
 use windows::Win32::UI::Input::KeyboardAndMouse::{GetKeyState, VK_CAPITAL, VK_NUMLOCK};
 
-fn main() {
-    let (tx, rx) = mpsc::channel();
+#[derive(Default, NwgUi)]
+pub struct LockIndicator {
+    #[nwg_control]
+    window: nwg::MessageWindow,
 
-    let mut tray = TrayItem::new("Caps/Num Lock Indicator", "state0-icon").expect("creating tray");
-    tray.add_menu_item("Quit", move || {
-        tx.send(true).expect("sending channel message");
-    }).expect("adding menu item");
+    #[nwg_resource]
+    embed: nwg::EmbedResource,
+
+    #[nwg_resource(source_embed: Some(&data.embed), source_embed_str: Some("STATE0ICON"))]
+    state0icon: nwg::Icon,
+    #[nwg_resource(source_embed: Some(&data.embed), source_embed_str: Some("STATE1ICON"))]
+    state1icon: nwg::Icon,
+    #[nwg_resource(source_embed: Some(&data.embed), source_embed_str: Some("STATE2ICON"))]
+    state2icon: nwg::Icon,
+    #[nwg_resource(source_embed: Some(&data.embed), source_embed_str: Some("STATE3ICON"))]
+    state3icon: nwg::Icon,
+
+    #[nwg_control(icon: Some(&data.state0icon), tip: Some("Caps/Num Lock Indicator"))]
+    #[nwg_events(MousePressLeftUp: [LockIndicator::show_menu], OnContextMenu: [LockIndicator::show_menu])]
+    tray: nwg::TrayNotification,
+
+    #[nwg_control(parent: window, popup: true)]
+    tray_menu: nwg::Menu,
+
+    #[nwg_control(parent: tray_menu, text: "Exit")]
+    #[nwg_events(OnMenuItemSelected: [LockIndicator::exit])]
+    tray_item: nwg::MenuItem,
+}
+
+impl LockIndicator {
+    fn change_icon(&self, state: u8) {
+        match state {
+            0 => self.tray.set_icon(&self.state0icon),
+            1 => self.tray.set_icon(&self.state1icon),
+            2 => self.tray.set_icon(&self.state2icon),
+            3 => self.tray.set_icon(&self.state3icon),
+            _ => panic!("state out of range"),
+        }
+    }
+
+    fn show_menu(&self) {
+        let (x, y) = nwg::GlobalCursor::position();
+        self.tray_menu.popup(x, y);
+    }
     
-    let mut last_state = 0;
+    fn exit(&self) {
+        nwg::stop_thread_dispatch();
+    }
+}
 
-    loop {
+fn main() {
+    nwg::init().expect("Failed to init Native Windows GUI");
+    let ui = LockIndicator::build_ui(Default::default()).expect("Failed to build UI");
+
+    let mut last_state = 0;
+    nwg::dispatch_thread_events_with_callback(move || {
         let mut state = 0;
 
         unsafe {
@@ -24,16 +71,8 @@ fn main() {
         }
 
         if last_state != state {
-            let mut string_icon = String::from("state");
-            string_icon.push_str(&state.to_string());
-            string_icon.push_str("-icon");
-            tray.set_icon(&string_icon).expect("switching icon");
-            last_state = state
+            ui.change_icon(state);
+            last_state = state;
         }
-
-        match rx.recv_timeout(Duration::from_millis(50)) {
-            Ok(_) => break,
-            Err(_) => continue,
-        }
-    }
+    });
 }
